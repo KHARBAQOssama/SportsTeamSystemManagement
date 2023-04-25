079b52489bbb67d8266e09d39746c6be73b4f16a
Ossama kharbaq <ossamakharbaq4@gmail.com>
2023-04-25 17:17:51 +0100

    adding new controller that called sport and i chenge the idea from adding sports by the admin th

---
+++ The_Back_End_Part_Rest_Api/app/Http/Controllers/BranchController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Branch;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\StoreOrUpdateBranchRequest;
+use App\Models\Tournament;
+
+class BranchController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $branches = Branch::with('users','tournaments','games')->get();
+        return response()->json($branches);   
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreOrUpdateBranchRequest $request)
+    {
+        $credentials = [
+            'name' => $request->input('name'),
+            'sport_id' => $request->input('sport_id'),
+        ];
+        
+        if ($request->input('icon')) {
+            $base64_string = $request->input('icon');
+            $image_data = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $base64_string));
+    
+            $filename = uniqid() . '.jpg';
+
+            Storage::put('public/images/' . $filename, $image_data);
+            $url = asset('storage/images/'.$filename);
+            $credentials['icon'] = $url;
+        }
+
+        $branch = Branch::create($credentials);
+        
+        return response()->json([
+            'message' => 'Branch added successfully'
+        ]);
+        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Branch $branch)
+    {
+        return response()->json($branch);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(StoreOrUpdateBranchRequest $request, Branch $branch)
+    {
+        $credentials = [
+            'name' => $request->input('name')
+        ];
+        
+        if ($request->input('image')) {
+            $base64_string = $request->input('image');
+            $image_data = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $base64_string));
+    
+            $filename = uniqid() . '.jpg';
+
+            Storage::put('public/images/' . $filename, $image_data);
+            $url = asset('storage/images/'.$filename);
+            $credentials['icon'] = $url;
+        }
+
+        $branch->update($credentials);
+        
+        return response()->json([
+            'message' => 'Branch updated successfully'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Branch $branch)
+    {
+        $branch->delete();
+        return response()->json([
+            'message' => 'Branch deleted successfully'
+        ]);
+    }
+
+    public function getBranchUsers(Branch $branch){
+        return response()->json($branch->users);
+    }
+
+    public function authenticatedBranchUsers(){
+        $users = JWTAuth::user()->Branch->users;
+        return response()->json($users);
+    }
+}
